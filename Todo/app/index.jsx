253f6017098fd0f37f628980507e4241d447ba77
Rashid <rashidfarzaliyev111@gmail.com>
2025-06-11 21:27:49 +0400

    Add date-time-picker

--- Todo/app/index.jsx
+++ Todo/app/index.jsx
@@ -10,8 +10,20 @@ import {
   View,
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function App() {
+  const [date, setDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    setShowPicker(false);
+    if (selectedDate) {
+      setDate(selectedDate);
+    }
+  };
+
+  const [add, setAdd] = useState();
   const [data, setData] = useState([
     {
       header: "University",
@@ -19,7 +31,11 @@ export default function App() {
     },
     {
       header: "Programming",
-      data: [{ title: "Do todo", priority: "2", checked: true }],
+      data: [{ title: "Do todo", priority: "2", checked: false }],
+    },
+    {
+      header: "Done",
+      data: [],
     },
   ]);
 
@@ -32,15 +48,15 @@ export default function App() {
   const [selectedPriorityFilter, setSelectedPriorityFilter] = useState("");
 
   function addTodo() {
-    if (titleInput === "") {
+    if (titleInput.trim() === "") {
       alert("Title cannot be empty");
       return;
     }
-    if (selectedHeader === "") {
+    if (selectedHeader.trim() === "") {
       alert("You need to select header");
       return;
     }
-    if (selectedPriority === "") {
+    if (selectedPriority.trim() === "") {
       alert("You need to select priority");
       return;
     }
@@ -51,7 +67,12 @@ export default function App() {
             ...item,
             data: [
               ...item.data,
-              { title: titleInput, priority: selectedPriority, checked: false },
+              {
+                title: titleInput,
+                priority: selectedPriority,
+                date: date,
+                checked: false,
+              },
             ],
           };
         }
@@ -74,12 +95,16 @@ export default function App() {
         if (item.header === header) {
           return {
             ...item,
-            data: item.data.map((iitem) => {
-              if (iitem.title === title) {
-                return { ...iitem, checked: !iitem.checked };
-              }
-              return iitem;
+            data: item.data.filter((iitem) => {
+              console.log(iitem);
+              return iitem.title !== title;
             }),
+          };
+        }
+        if (header !== "Done" && item.header === "Done") {
+          return {
+            ...item,
+            data: [...item.data, { title, checked: true }],
           };
         }
         return item;
@@ -89,65 +114,111 @@ export default function App() {
 
   return (
     <View style={styles.main}>
-      <TextInput
-        placeholder="Title"
-        style={styles.input}
-        onChangeText={setTitleInput}
-        value={titleInput}
-      />
-      <Picker
-        selectedValue={selectedHeader}
-        onValueChange={(itemValue) => setSelectedHeader(itemValue)}
-      >
-        <Picker.Item label={"Header"} value={""} />
-        {data.map((item, i) => (
-          <Picker.Item
-            key={i}
-            label={item.header[0].toUpperCase() + item.header.slice(1)}
-            value={item.header}
+      <View style={styles.top}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <Button title="Add todo" onPress={() => setAdd("todo")} />
+          <Button title="Add header" onPress={() => setAdd("header")} />
+        </View>
+        <View
+          style={[
+            styles.addMenu,
+            { display: add === "todo" ? "contents" : "none" },
+          ]}
+        >
+          <TextInput
+            placeholder="Title"
+            style={styles.input}
+            onChangeText={setTitleInput}
+            value={titleInput}
           />
-        ))}
-      </Picker>
-      <Picker
-        selectedValue={selectedPriority}
-        onValueChange={(itemValue) => setSelectedPriority(itemValue)}
-      >
-        <Picker.Item label="Priority" value={""} />
-        <Picker.Item label="High" value={3} />
-        <Picker.Item label="Medium" value={2} />
-        <Picker.Item label="Low" value={1} />
-      </Picker>
-      <Button title="Add todo" onPress={addTodo} />
-      <TextInput
-        placeholder="Header"
-        style={styles.input}
-        onChangeText={setHeaderInput}
-        value={headerInput}
-      />
-      <Button title="Add header" onPress={addHeader} />
+          <Picker
+            selectedValue={selectedHeader}
+            onValueChange={(itemValue) => setSelectedHeader(itemValue)}
+          >
+            <Picker.Item label={"Header"} value={""} />
+            {data.map((item, i) => (
+              <Picker.Item
+                key={i}
+                label={item.header[0].toUpperCase() + item.header.slice(1)}
+                value={item.header}
+              />
+            ))}
+          </Picker>
+          <Picker
+            selectedValue={selectedPriority}
+            onValueChange={(itemValue) => setSelectedPriority(itemValue)}
+          >
+            <Picker.Item label="Priority" value={""} />
+            <Picker.Item label="High" value={"3"} />
+            <Picker.Item label="Medium" value={"2"} />
+            <Picker.Item label="Low" value={"1"} />
+          </Picker>
+          <Button title="Date" onPress={() => setShowPicker(true)} />
+          <View style={{ height: 3 }}></View>
+          {showPicker && (
+            <DateTimePicker
+              value={date}
+              mode="date"
+              onChange={onChange}
+            />
+          )}
+          <Button title="Submit" onPress={addTodo} />
+          <View style={{ height: 3 }}></View>
+          <Button title="Close" onPress={() => setAdd("")} />
+        </View>
+        <View
+          style={[
+            styles.addMenu,
+            { display: add === "header" ? "contents" : "none" },
+          ]}
+        >
+          <View>
+            <TextInput
+              placeholder="Header"
+              style={styles.input}
+              onChangeText={setHeaderInput}
+              value={headerInput}
+            />
+            <Button title="Submit" onPress={addHeader} />
+            <View style={{ height: 3 }}></View>
+            <Button title="Close" onPress={() => setAdd("")} />
+          </View>
+        </View>
+      </View>
       <Text style={styles.heading}>Filters</Text>
-      <Picker
-        selectedValue={selectedPriorityFilter}
-        onValueChange={(itemValue) => setSelectedPriorityFilter(itemValue)}
-      >
-        <Picker.Item label="Priority" value={""} />
-        <Picker.Item label="High" value={3} />
-        <Picker.Item label="Medium" value={2} />
-        <Picker.Item label="Low" value={1} />
-      </Picker>
-      <Picker
-        selectedValue={selectedHeaderFilter}
-        onValueChange={(itemValue) => setSelectedHeaderFilter(itemValue)}
-      >
-        <Picker.Item label={"Header"} value={""} />
-        {data.map((item, i) => (
-          <Picker.Item
-            key={i}
-            label={item.header[0].toUpperCase() + item.header.slice(1)}
-            value={item.header}
-          />
-        ))}
-      </Picker>
+      <View styles={styles.filters}>
+        <View style={styles.filter}>
+          <Picker
+            selectedValue={selectedPriorityFilter}
+            onValueChange={(itemValue) => setSelectedPriorityFilter(itemValue)}
+          >
+            <Picker.Item label="Priority" value={""} />
+            <Picker.Item label="High" value={3} />
+            <Picker.Item label="Medium" value={2} />
+            <Picker.Item label="Low" value={1} />
+          </Picker>
+        </View>
+        <View style={styles.filter}>
+          <Picker
+            selectedValue={selectedHeaderFilter}
+            onValueChange={(itemValue) => setSelectedHeaderFilter(itemValue)}
+          >
+            <Picker.Item label={"Header"} value={""} />
+            {data.map((item, i) => (
+              <Picker.Item
+                key={i}
+                label={item.header[0].toUpperCase() + item.header.slice(1)}
+                value={item.header}
+              />
+            ))}
+          </Picker>
+        </View>
+      </View>
       <SectionList
         sections={data
           .map((item) => {
@@ -165,7 +236,6 @@ export default function App() {
             };
           })
           .filter((item) => {
-            console.log(selectedHeaderFilter);
             if (
               selectedHeaderFilter !== "" &&
               item.header !== selectedHeaderFilter
@@ -176,7 +246,7 @@ export default function App() {
           })}
         keyExtractor={(item, index) => item + index}
         renderItem={({ item, section }) => {
-          let style = styles.lowPriority;
+          let style = { backgroundColor: "grey" };
           switch (item.priority) {
             case "1":
               style = styles.lowPriority;
@@ -202,12 +272,22 @@ export default function App() {
                 >
                   {item.title}
                 </Text>
+                <Text style={styles.title}>
+                  {item.date?.toString()}
+                </Text>
               </View>
             </TouchableWithoutFeedback>
           );
         }}
         renderSectionHeader={({ section: { header } }) => (
-          <Text style={styles.header}>{header}</Text>
+          <Text
+            style={[
+              styles.header,
+              header === "Done" ? { color: "grey", fontStyle: "italic" } : {},
+            ]}
+          >
+            {header}
+          </Text>
         )}
       />
     </View>
@@ -216,7 +296,23 @@ export default function App() {
 
 const styles = StyleSheet.create({
   main: {
+    flex: 1,
     paddingTop: StatusBar.currentHeight,
+    margin: 8,
+  },
+  top: {
+    flexDirection: "col",
+  },
+  filters: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  filter: {
+    backgroundColor: "lightblue",
+  },
+  addMenu: {
+    flex: 1,
+    justifyContent: "flex-end",
   },
   heading: {
     fontSize: 20,
@@ -245,6 +341,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   input: {
+    marginVertical: 5,
+    borderRadius: 15,
+    borderWidth: 1,
     textAlign: "center",
     fontSize: 16,
   },
